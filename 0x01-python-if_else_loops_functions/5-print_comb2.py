a01693758fa7ab03fca6f1d957a52fc4d261f839
abrahamof2016 <jarageda9@gmail.com>
2023-11-11 17:52:43 +0000

    prints a number from 0 to 99

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
-for i in range(0, 10):
-    for j in range(0, 10):
-        print("{:d}{:d} ".format(i, j), end='')
+for i in range(0, 100):
+    if (i != 99):
+        print("{:02d}, ".format(i), end='')
+    else:
+        print("{:d}".format(i))
