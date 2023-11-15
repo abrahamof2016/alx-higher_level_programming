ae87184b302c35d4260e2186def557685227da5a
abrahamof2016 <jarageda9@gmail.com>
2023-11-15 08:51:39 +0000

    make it executable

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,6 +2,6 @@
 def print_reversed_list_integer(my_list=[]):
     if not my_list:
         return
-    my_list.reverse()    
+    my_list.reverse()
     for i in my_list:
         print("{:d}".format(i))
