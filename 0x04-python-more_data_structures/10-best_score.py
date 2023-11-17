db22427d499656d7fb341e980ecce64bfea85c06
abrahamof2016 <jarageda9@gmail.com>
2023-11-17 12:54:39 +0000

    return a key with the biggest integer value

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -5,7 +5,7 @@ def best_score(a_dictionary):
     else:
         best_score = 0
         for key in a_dictionary:
-            if a_dictionary[key] > best_score:
+            if a_dictionary[key] >= best_score:
                 best_score = a_dictionary[key]
                 best_scorer = key
     return best_scorer
