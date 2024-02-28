bea241c6c8d49efb9b3b0d31c24404703ea9a7d4
abrahamof2016 <jarageda9@gmail.com>
2024-02-28 12:06:07 +0000

    creates a table called first_table

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,7 @@
+-- A script to create a table called first_table in the current database.
+USE hbtn_0c_0;
+DROP TABLE IF EXISTS first_table;
+CREATE TABLE first_table(
+    id INTEGER,
+    name VARCHAR(256)
+);
