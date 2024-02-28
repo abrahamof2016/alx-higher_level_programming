7457a44cede884ad79af9cbf9031910febda1092
abrahamof2016 <jarageda9@gmail.com>
2024-02-28 21:39:24 +0000

    create a second_table

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,13 @@
+-- A script that creates a table second_table.
+DROP TABLE IF EXISTS second_table;
+CREATE TABLE second_table(
+    id INTEGER,
+    name VARCHAR(256),
+    score INTEGER);
+INSERT INTO second_table
+VALUES
+(1, "John", 10),
+(2, "ALEX", 3),
+(3, "Bob", 14),
+(4, "George", 8)
+;
