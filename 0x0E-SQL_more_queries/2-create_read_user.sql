689aa73cd8e0e565d0181eee4b963f0813f377c9
abrahamof2016 <jarageda9@gmail.com>
2024-03-04 18:02:01 +0000

    add a new line at the end

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,7 +1,8 @@
--- A Script that creates the database hbtn_0d_2 and user user_od_2 
+-- A Script that creates the database hbtn_0d_2 and user user_od_2
 DROP USER IF EXISTS 'user_0d_2'@'localhost';
 CREATE USER 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 DROP DATABASE IF EXISTS hbtn_0d_2;
 CREATE DATABASE hbtn_0d_2;
 GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;
 FLUSH PRIVILEGES;
+
