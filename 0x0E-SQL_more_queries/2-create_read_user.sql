0f9c4334e428dffd2339d6add8f011f44407e1ed
Abraham <jarageda9@gmail.com>
2024-03-04 21:00:21 +0300

    update comment to singe line comment

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,4 @@
-/* A Script that creates the database hbtn_0d_2 and user user_od_2 */
+-- A Script that creates the database hbtn_0d_2 and user user_od_2 
 DROP USER IF EXISTS 'user_0d_2'@'localhost';
 CREATE USER 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 DROP DATABASE IF EXISTS hbtn_0d_2;
