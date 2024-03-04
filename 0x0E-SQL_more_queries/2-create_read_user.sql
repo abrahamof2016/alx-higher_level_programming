8636f46e49d592494d5a36099ce935aca4452c85
abrahamof2016 <jarageda9@gmail.com>
2024-03-04 18:08:08 +0000

    update 2-create_read_user.sql

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,8 +1,5 @@
 -- A Script that creates the database hbtn_0d_2 and user user_od_2
-DROP USER IF EXISTS 'user_0d_2'@'localhost';
-CREATE USER 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-DROP DATABASE IF EXISTS hbtn_0d_2;
-CREATE DATABASE hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 FLUSH PRIVILEGES;
-
