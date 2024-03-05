a6748c9e334d30f647d8a28ec32214421c0d5ab0
abrahamof2016 <jarageda9@gmail.com>
2024-03-05 12:47:48 +0000

    create a table with foreign key

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,11 @@
+-- A script that creates the database hbtn_0d_usa and the table cities
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities(
+    id INTEGER AUTO_INCREMENT NOT NULL,
+    state_id INTEGER NOT NULL,
+    name VARCHAR(256) NOT NULL,
+    PRIMARY KEY(id)
+    FOREIGN KEY(state_id)
+        REFERENCES states(id)
+    )ENGINE = InnoDB;
