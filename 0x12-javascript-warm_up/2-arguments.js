dd9cbd402014c27bece02750af1870bba4ddad0a
abrahamof2016 <jarageda9@gmail.com>
2024-03-13 18:49:17 +0000

    fix semistandard errors

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,15 +1,10 @@
 #!/usr/bin/node
-const { argv } = require(`node:process`);
-let args = process.argv;
-if (args.length == 2)
-{
-    console.log("No argument");
-}
-else if (args.length == 3)
-{
-    console.log("Argument found");
-}
-else
-{
-    console.log("Arguments found");
+const process = require('node:process');
+const args = process.argv;
+if (args.length === 2) {
+  console.log('No argument');
+} else if (args.length === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }
