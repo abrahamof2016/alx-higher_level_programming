818a0ffd9511bd273bf54b785af2a66d3307ed98
abrahamof2016 <jarageda9@gmail.com>
2024-03-14 06:06:54 +0000

    prints 3 lines

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
-let array = ["C is fun", "Python is cool", "JavaScript is amazing"];
+const array = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 let i = 0;
-while (i < array.length){
-    console.log(array[i]);
-    i++;}
+while (i < array.length) {
+  console.log(array[i]);
+  i++;
+}
