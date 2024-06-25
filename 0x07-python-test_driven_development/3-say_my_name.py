835d3a381ce0880f32e814dbf619eeed3f4f5551
abrahamof2016 <jarageda9@gmail.com>
2024-06-25 17:07:49 +0000

    print full name

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+module to print full name.
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """
+    arg:
+        first_name: a first name(string)
+        last_name: a second name(string)
+        return: nothing.
+    """
+    if not isinstance(first_name, str) or first_name is None:
+        raise TypeError(
+                'first_name must be a string'
+                )
+    if not isinstance(last_name, str) or last_name is None:
+        raise TypeError(
+                'last_name must be a string'
+                )
+    print(first_name + " " + last_name)
